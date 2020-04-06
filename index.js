64e2460e168a487f8f8e8b1289afb583ad890a76
royhan31 <izzapark34@gmail.com>
2020-04-07 06:36:16 +0700

    new scrap tegal

--- index.js
+++ index.js
@@ -49,24 +49,6 @@ app.get('/', async function (req, res) {
     return requestPromise.get(url,{strictSSL: false}, (err,body) => body);
   }
 
-  //  request({
-  //   method: 'GET',
-  //   url: 'http://corona.brebeskab.go.id/'
-  //   }, function(err, response, body, callback) {
-  //     if (err) return console.error(err);
-  //
-  //
-  //
-  //     if(coronaBrebes.length > 0){
-  //       coronaBrebes = [];
-  //     }
-  //
-  //
-  // });
-
-  // var coronaArray = coronaBrebes
-
-
 });
 
 
@@ -88,26 +70,54 @@ app.get('/tegal', async function (req, res) {
       const $ = cheerio.load(results);
       const content = $('body .content');
 
-      const text = [];
-      const num = [];
+      const title = [];
+      const total = [];
+      const finish = [];
       const dataKec = [];
       const tableHeaders = [];
       const rsText = [];
+      const monitoring = [];
+      const details = [];
 
-      content.find('div .inner p').each(function (i){ text[i] = $(this).text().replace(' (Pelaku Perjalanan)','').replace(' (Orang Tanpa Gejala)','') });
-      content.find('div .inner h3').each(function (i){ num[i] = $(this).text().trim().replace(/  +/g, ' ') });
-      $('body > section > div > div.col-md-4 > div > div.panel-body > table > thead > tr').each((index, element) => {
+
+
+      $('body > section > div > div > div > div.inner > div > div.widget-user-header > center > h5 > strong').each((index, element) => {
+        let text = $(element).text().replace(' (Pelaku Perjalanan)','').replace(' (Orang Tanpa Gejala)','').replace(' (Orang Dalam Pantauan)','').replace(' (Pasien Dalam Pengawasan)','');
+        title.push(text)
+      })
+      $('body > section > div > div > div > div.inner > div > div.widget-user-header > center > h4 > strong').each((index, element) => {
+        let text = $(element).text().replace('TOTAL','.').replace('.','');
+        total.push(text);
+      });
+
+      $('body > section > div > div > div > div.inner > div > div.card-footer > div > div.col-sm-6.border-right > div > span').each((index, element) => {
+        let text = $(element).text();
+        finish.push(text)
+      });
+
+      $('body > section > div > div > div > div.inner > div > div.card-footer > div > div > div > span').each((index, element) => {
+        let text = $(element).text();
+        monitoring.push(text)
+      });
+
+      $('body > section > div > div > div > div.inner > div > div.card-footer > div > div > div > span').each((index, element) => {
+        let text = $(element).text();
+        details.push(text)
+      });
+
+
+        $('body > section > div > div.col-md-4 > div > div.panel-body > table > thead > tr').each((index, element) => {
         if (index === 0) {
           const ths = $(element).find("th");
            $(ths).each((i, element) => {
-             tableHeaders.push(
-               $(element)
-                 .text()
-             );
+             tableHeaders.push($(element).text());
            });
+
             return true;
           }
       });
+
+
 
       $('body > section > div > div.col-md-4 > div > div.panel-body > table > tbody > tr').each((index, element) => {
           const tds = $(element).find('td');
@@ -130,21 +140,44 @@ app.get('/tegal', async function (req, res) {
       const rsData = rsText.map(r => r.substring(3)).filter((r,i) => i >= 0 && i <= 4 );
       const rsKet = rsText.filter((r,i) => i >= 6 && i <= 9);
 
+      const totalData = total.filter(t => t !== '');
+      const dataMonitoring = monitoring.filter((m,i) => i === 1 || i === 3 || i === 5);
 
-      const confirm = Object.assign(...text.map((t,i) => ({[t.replace(' ','_')] : +num[i]}) ));
+      const detailData = details.filter((d,i) => i > 5);
+
+      const data = [];
+      title.map((r,i) => {
+        if(i < 3) {
+          data.push({[r] : {
+            total: totalData[i],
+            selesai : finish[i],
+            pantauan: dataMonitoring[i]
+            }
+          })
+        }else {
+          data.push({[r] : {
+            total: totalData[i],
+            sembuh: i === 3 ? detailData[0] : detailData[3],
+            dirawat: i === 3 ? detailData[1] : detailData[4],
+            meniggal: i === 3 ? detailData[2] : detailData[5]
+            }
+          })
+        }
+      })
+
+      resultData = Object.assign({}, ...data.map((d,i) => d ))
 
       res.send(JSON.stringify({
         message: 'success',
         status: true,
-        data: {
-          konfirmasi: confirm,
-          kecamatan: dataKec,
-          rs: {
-            data : rsData,
-            keterangan : rsKet
-          },
-          updated: update
-        }
+        data:
+        resultData,
+        kecamatan: dataKec,
+        rs: {
+          data : rsData,
+          keterangan : rsKet
+        },
+        updated: update
       }));
     } catch (e) {
       console.log(e);
